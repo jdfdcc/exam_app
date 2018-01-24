26a3847bd457ac1554af4a45c4ecc8e880f73b05
江道丰 <jiangdaofeng@n22.com.cn>
2018-01-24 11:09:57 +0800

    update

--- src/pages/exam/componts/answerPop.vue
+++ src/pages/exam/componts/answerPop.vue
@@ -5,9 +5,15 @@
         <div class="bg-primary-content pd-md" style="">提醒：点击题目序号，可切换至相应题目</div>
         <section class="answer_content">
           <h3>单项选择器</h3>
-          <div class="answer_list font-hg" v-bind:style="{'max-height':screenHeight - 130+'px'}">
-            <mu-raised-button @click="toItem(item,index)" style="font-weight:100" v-bind:class="[item.value!=100?'bg-primary':'']" v-for="(item,index) in $parent.swiperSlides" :key="index" :label="index+1+''" class="answer_button  " />
+
+          <div class="menu_question">
+            <div @click="menuIndex(item)" v-for="(item,index) in menu" :key="index" class="menu_question_answer">{{item*menuItemList + 1}}-{{(index+1)*menuItemList}}</div>
           </div>
+
+          <div class="answer_list font-hg" v-bind:style="{'max-height': screenHeight =  200 + 'px'}">
+            <mu-raised-button @click="toItem(item.detail,item.id)" style="font-weight:100" v-bind:class="[item.detail.value != 100?'bg-primary':'']" v-for="(item,index) in indexList" :key="index" :label="item.id - 0 + 1 + ''" class="answer_button  " />
+          </div>
+
         </section>
         <mu-raised-button @click="$parent.showAnserPop = false" class="button-second" label="我知道了" />
       </mu-content-block>
@@ -28,11 +34,46 @@ export default {
   },
   data() {
     return {
+      menu:[],
+      indexList:[],
+      menuItemList:50
     }
   },
   methods: {
     toItem(item,index) {
       this.$emit("toQus", item,index);
+    },
+    menuIndex(index){
+      this.indexList = []
+      if (index != this.menu.length -1 || this.$parent.swiperSlides.length % this.menuItemList ==0){
+        for(let i = 0; i<this.menuItemList; i++){
+          let itemIndex = index*this.menuItemList +i;
+          this.indexList.push({
+            id:itemIndex,
+            detail:this.$parent.swiperSlides[itemIndex]
+          })
+        }
+      }else{
+        // 当为最后一个划分的时候
+        let length = this.$parent.swiperSlides % this.menuItemList
+        for(let i = 0; i < length; i++){
+          let itemIndex = index*this.menuItemList +i;
+          this.indexList.push({
+            id:itemIndex,
+            detail:this.$parent.swiperSlides[itemIndex]
+          })
+        }
+      }
+    }
+  },
+  watch: {
+    '$parent.showAnserPop'(){
+      this.menu = []
+      let index = Math.ceil(this.$parent.swiperSlides.length / (this.menuItemList+1))
+      console.log(index)
+      for(let i =0;i<index;i++){
+        this.menu.push(i)
+      }
     }
   }
 }
@@ -40,6 +81,25 @@ export default {
 
 <style rel="stylesheet/scss" lang="scss" >
 .answer_pop {
+  .menu_question{
+    display: flex;
+    height: 60px;
+    overflow-x: scroll;
+    width: 100vw;
+    // flex-wrap: wrap;
+    border-bottom: 1px solid rgba(0,0,0,.5);
+    padding-bottom: 10px;
+    .menu_question_answer{
+      min-width: 90px;
+      width:80px;
+      font-size: 13px;
+      padding: 10px;
+      border: 1px solid rgba(0,0,0,.5);
+      border-radius: 3px;
+      text-align: center;
+      margin: 10px 0px 0px 10px;
+    }
+  }
   .button-second {
     bottom: 0px;
     left: 0px;
